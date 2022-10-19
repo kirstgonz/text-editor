2d549eac4c81564b6b728609b81761ff5805191b
kirstgonz <104031234+kirstgonz@users.noreply.github.com>
2022-10-19 01:04:30 -0500

    updated install button in index.js in client src folder

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -32,3 +32,21 @@ if ('serviceWorker' in navigator) {
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
+  // Install button 
+  const installBtn = document.getElementById('buttonInstall');
+
+  window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    buttonInstall.style.visibility = 'visible';
+  
+    installBtn.addEventListener('click', () => {
+      event.prompt();
+      installBtn.setAttribute('disabled', true);
+      installBtn.textContent = 'Installed!';
+    });
+  });
+  
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+  });
