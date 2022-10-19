b9f8c72ae998fe659b91d4cc40ff04dfb877139b
kirstgonz <104031234+kirstgonz@users.noreply.github.com>
2022-10-19 00:27:25 -0500

    updated database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (textEditorContent) => {
+  console.error('PUT to db');
+  const jateDb = await openDB('jate_db', 1);
+
+  const tx = jateDb.transaction('jate_db', 'readwrite');
+
+  const store = tx.objectStore('text');
+  
+  const request = store.put({ textEditorContent });
+  const result = await request;
+  console.log('Saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.error('GET from db');
+
+  // connects db and version
+  const jateDb = await openDB('jate_db', 1);
+
+  const tx = jateDb.transaction('jate_db', 'readonly');
+
+  // open object store
+  const store = tx.objectStore('text');
+
+  // get all info in db
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
